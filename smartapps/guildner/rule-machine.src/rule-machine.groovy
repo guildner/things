c2f97dde984816236eb9c4af29a3d2cd83d05e4f
Luke Guildner <luke.guildner@gmail.com>
2016-02-09 12:41:40 -0700

    Making the apps mine, so they work...

---
+++ smartapps/guildner/rule-machine.src/rule-machine.groovy
@@ -0,0 +1,38 @@
+/**
+ *  Rule Machine
+ *
+ *  Copyright 2016 Luke Guildner
+ *
+ */
+definition(
+    name: "Rule Machine",
+    namespace: "guildner",
+    author: "Luke Guildner",
+    description: "A rule machine.",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+    page(name: "mainPage", title: "Rules", install: true, uninstall: true,submitOnChange: true) {
+            section {
+                    app(name: "childRules", appName: "Rule", namespace: "bravenel", title: "Create New Rule...", multiple: true)
+            }
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    childApps.each {child ->
+            log.info "Installed Rules: ${child.label}"
+    }
+}
